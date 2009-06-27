0878802ed6c598e205ab624a0431fb2458d6417f
Ari Lerner <arilerner@mac.com>
2009-06-26 17:02:48 -0700

    Initial checkin

---
+++ src/REST/rest_server.erl
@@ -0,0 +1,118 @@
+%%%-------------------------------------------------------------------
+%%% File    : rest_server.erl
+%%% Author  : Ari Lerner
+%%% Description : 
+%%%
+%%% Created :  Fri Jun 26 17:00:39 PDT 2009
+%%%-------------------------------------------------------------------
+
+-module (rest_server).
+-behaviour(gen_server).
+
+%% API
+-export([start_link/0]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+-record(state, {
+        }).
+-define(SERVER, ?MODULE).
+
+%%====================================================================
+%% API
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Function: start_link() -> {ok,Pid} | ignore | {error,Error}
+%% Description: Starts the server
+%%--------------------------------------------------------------------
+start_link() ->
+  gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+%%====================================================================
+%% gen_server callbacks
+%%====================================================================
+
+%%--------------------------------------------------------------------
+%% Function: init(Args) -> {ok, State} |
+%%                         {ok, State, Timeout} |
+%%                         ignore               |
+%%                         {stop, Reason}
+%% Description: Initiates the server
+%%--------------------------------------------------------------------
+init([Port]) ->
+  mochiweb_http:start([ {port, Port},
+                        {loop, fun dispatch_requests/1}]),
+  {ok, #state{}}.
+
+%%--------------------------------------------------------------------
+%% Function: %% handle_call(Request, From, State) -> {reply, Reply, State} |
+%%                                      {reply, Reply, State, Timeout} |
+%%                                      {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, Reply, State} |
+%%                                      {stop, Reason, State}
+%% Description: Handling call messages
+%%--------------------------------------------------------------------
+handle_call(_Request, _From, State) ->
+  Reply = ok,
+  {reply, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: handle_cast(Msg, State) -> {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, State}
+%% Description: Handling cast messages
+%%--------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+  {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: handle_info(Info, State) -> {noreply, State} |
+%%                                       {noreply, State, Timeout} |
+%%                                       {stop, Reason, State}
+%% Description: Handling all non call/cast messages
+%%--------------------------------------------------------------------
+handle_info(_Info, State) ->
+  {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: terminate(Reason, State) -> void()
+%% Description: This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any necessary
+%% cleaning up. When it returns, the gen_server terminates with Reason.
+%% The return value is ignored.
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+  ok.
+
+%%--------------------------------------------------------------------
+%% Func: code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% Description: Convert process state when code is changed
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+
+%%--------------------------------------------------------------------
+%%% Internal functions
+%%--------------------------------------------------------------------
+
+dispatch_requests(Req) ->
+  Path = Req:get(path),
+  Action = clean_path(Path),
+  handle(Action, Req).
+  
+handle(Unknown, Req) ->
+  Req:respond({404, [{"Content-Type", "text/plain"}], subst("Unknown action: ~s", [Unknown])}).
+
+subst(Template, Values) when is_list(Values) ->
+  list_to_binary(lists:flatten(io_lib:fwrite(Template, Values))).
+
+% Get a clean path
+% strips off the query string
+clean_path(Path) ->
+  case string:str(Path, "?") of
+    0 -> Path;
+    N -> string:substr(Path, 1, string:len(Path) - (N+1))
+  end.
---
+++ src/REST/rest_server_test.erl
@@ -0,0 +1,17 @@
+-module (rest_server_test).
+
+-export([get_user/0, update_user/0, delete_user/0, create_user/0]).
+
+get_user() ->
+    http:request(get, {"http://localhost:999/user/12345", []}, [], []).
+
+update_user() ->
+    Data = "<user xmlns=\"urn:userapp:user\"><id>12345</id><name>Testy McTester</name><email>testy@mctestor.com</email><password>testy123</password><website>http://www.mctestor.com/</website></user>",
+    http:request(post, {"http://localhost:9999/user/12345", [], "application/xml", Data}, [], []).
+
+delete_user() ->
+    http:request(delete, {"http://localhost:9999/user/12345", []}, [], []).
+
+create_user() ->
+    Data = "<user xmlns=\"urn:userapp:user\"><name>Testy McTester</name><email>testy.mctestor@mctestor.com</email><password>testy123</password></user>",
+    http:request(put, {"http://localhost:9999/user/", [], "application/xml", Data}, [], []).
