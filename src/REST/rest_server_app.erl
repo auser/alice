bd0559989f7c041bf562ce4b9b1714bcc50e411a
Ari Lerner <arilerner@mac.com>
2009-06-26 17:10:15 -0700

    Bah

--- src/REST/rest_server_app.erl
+++ src/REST/rest_server_app.erl
@@ -5,10 +5,12 @@
 -include("rest_server.hrl").
 
 start(_Type, _Args) ->
+  io:format("Starting... : ~p~n", application:get_env(?MODULE, port)),
     Args = lists:map(
         fun (Var) -> {ok, Value} = application:get_env(?MODULE, Var), Value end,
         [port]
     ),
-    rest_server_sup:start_link(Args).
+  io:format("Args: ~p~n", [Args]),
+  rest_server_sup:start_link(Args).
 
 stop(_State) -> ok.
