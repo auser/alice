ec8fc58ab92ddcfb830abd3de52b46d7834e582d
Ari Lerner <arilerner@mac.com>
2009-06-29 13:03:23 -0700

    Added start_link to take args

--- src/rest/rest_server.erl
+++ src/rest/rest_server.erl
@@ -10,7 +10,7 @@
 -behaviour(gen_server).
 
 %% API
--export([start_link/0]).
+-export([start_link/1]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
@@ -28,8 +28,8 @@
 %% Function: start_link() -> {ok,Pid} | ignore | {error,Error}
 %% Description: Starts the server
 %%--------------------------------------------------------------------
-start_link() ->
-  gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+start_link(Args) ->
+  gen_server:start_link({local, ?SERVER}, ?MODULE, [Args], []).
 
 %%====================================================================
 %% gen_server callbacks
@@ -42,7 +42,7 @@ start_link() ->
 %%                         {stop, Reason}
 %% Description: Initiates the server
 %%--------------------------------------------------------------------
-init([]) ->
+init([Args]) ->
   io:format("Starting ~p~n", [?MODULE]),
   {ok, #state{}}.
 
