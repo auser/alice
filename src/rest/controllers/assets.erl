9c4cd14a6d5baec6254a15721ca285e918a55355
Ari Lerner <arilerner@mac.com>
2009-07-14 18:50:26 -0700

    Added templating assets message logging

--- src/rest/controllers/assets.erl
+++ src/rest/controllers/assets.erl
@@ -6,7 +6,8 @@
 get(["css", Path]) -> {"text/css", read_css(Path)};
 get(["js", Path]) -> {"application/javascript", read_js(Path)};
 get(["images", Path]) -> {"application/binary", read_image(Path)};
-get(_Path) -> {"text/html", "Box"}.
+get(["templates", Path]) -> {"application/binary", read_template(Path)};
+get(Path) -> {"text/html", "unknown assets path: " ++ Path}.
 
 read_css(Name) ->
   {ok, Contents} = file:read_file(?WONDERLAND_DIR ++ "css/" ++ Name),
@@ -18,4 +19,8 @@ read_js(Name) ->
 
 read_image(Name) ->
   {ok, Contents} = file:read_file(?WONDERLAND_DIR ++ "images/" ++ Name),
+  Contents.
+  
+read_template(Name) ->
+  {ok, Contents} = file:read_file(?WONDERLAND_DIR ++ "templates/" ++ Name),
   Contents.
