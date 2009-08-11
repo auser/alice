78c1a3058f5ad4bea6505a5297cf9934a0a14899
Ari Lerner <arilerner@mac.com>
2009-08-11 10:43:57 -0700

    Added version to the controllers

--- include/alice.hrl
+++ include/alice.hrl
@@ -1,2 +1,3 @@
 -define(SOFTWARE_NAME, "------------- Alice ------------").
 -define(COPYRIGHT_MESSAGE, "Copyright (C) 2009 Ari Lerner, Nate Murray, Michael Fairchild, CloudTeam").
+-define (VERSION, "0.1a").
