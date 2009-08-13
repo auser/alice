bb36b941dfc3204e74bd99255a46b748e73ac78b
Ari Lerner <arilerner@mac.com>
2009-08-12 18:32:34 -0700

    Using try block instead of receive block

--- src/rest/controllers/permissions.erl
+++ src/rest/controllers/permissions.erl
@@ -12,15 +12,16 @@
 get([]) -> ?MODULE:get(["vhost", "/"]);
 get(["vhost", "root"]) -> ?MODULE:get(["vhost", "/"]);
 get(["vhost", Vhost]) ->
-  case rabint:call({rabbit_access_control, list_vhost_permissions, [Vhost]}) of
-    {error, {Atom, _Error}} -> {?MODULE, Atom};
-    {child_error, function_clause} -> 
+  try rabint:call({rabbit_access_control, list_vhost_permissions, [Vhost]}) of
+    {child_error, function_clause} ->
       ?ERROR("DEPRECATED SUPPORT: To get rid of this message, upgrade to RabbitMQ 1.6", []),
       list_vhost_users(Vhost);
-    % Jsonable = [{struct, [{"applications", Apps}, {"nodes", Nodes}, {"running_nodes", RunningNodes}]}],
+    {error, {Atom, _Error}} -> {?MODULE, Atom};
     Bin -> {"permissions", [erlang:tuple_to_list(P) || P <- Bin ]}
+  catch
+    _X -> {?MODULE, <<"unknown error">>}
   end;
-  
+    
 get([Username]) -> 
   get_user_perms(Username);
 get(Path) -> {"error", erlang:list_to_binary("unhandled: "++Path)}.
@@ -30,43 +31,49 @@ post([Username], Data) ->
   CPerm = extract_param("configure", Data),
   WPerm = extract_param("write", Data),
   RPerm = extract_param("read", Data),
-  case rabint:call({rabbit_access_control, set_permissions, [Username, VHost, CPerm, WPerm, RPerm]}) of
-    {error, {Atom, _Error}} -> {?MODULE, Atom};
-    {child_error, function_clause} -> 
+  
+  try rabint:call({rabbit_access_control, set_permissions, [Username, VHost, CPerm, WPerm, RPerm]}) of
+    {child_error, function_clause} ->
       ?ERROR("DEPRECATED SUPPORT: To get rid of this message, upgrade to RabbitMQ 1.6", []),
       map_user_to_vhost(Username, VHost);
+    {error, {Atom, _Error}} -> {?MODULE, Atom};
     ok -> get_user_perms(Username)
+  catch
+    _X -> {?MODULE, <<"unknown error">>}
   end;
-  
+    
 post(_Path, _Data) -> {"error", <<"unhandled">>}.
 put(_Path, _Data) -> {"error", <<"unhandled">>}.
 
 delete(["/", Username], Data) ->
-  VHost = extract_vhost(Data),
-  case rabint:call({rabbit_access_control, clear_permissions, [Username, VHost]}) of    
-    {child_error, function_clause} -> 
+  VHost = extract_vhost(Data),  
+  try rabint:call({rabbit_access_control, clear_permissions, [Username, VHost]}) of
+    {child_error, function_clause} ->
       ?ERROR("DEPRECATED SUPPORT: To get rid of this message, upgrade to RabbitMQ 1.6", []),
       unmap_user_from_vhost(Username, VHost);
-    {Error, _} -> {?MODULE, Error};
+    {error, {Atom, _Error}} -> {?MODULE, Atom};
     ok -> get_user_perms(Username)
+  catch
+    _X -> {?MODULE, <<"unknown error">>}
   end;
+  
   
 delete(_Path, _Data) -> {"error", <<"unhandled">>}.
 
 % PRIVATE
 get_user_perms(Username) ->
-  case rabint:call({rabbit_access_control, list_user_permissions, [Username]}) of    
-    {child_error, function_clause} -> 
+  try rabint:call({rabbit_access_control, list_user_permissions, [Username]}) of
+    {child_error, function_clause} ->
       ?ERROR("DEPRECATED SUPPORT: To get rid of this message, upgrade to RabbitMQ 1.6", []),
       list_user_vhosts(Username);
-      
     {_Error, Reason} -> 
       ?ERROR("Got error in rabint call for get_user_perms: ~p~n", [Reason]),
       {?MODULE, erlang:list_to_binary("unknown user: "++Username)};
-    % Jsonable = [{struct, [{"applications", Apps}, {"nodes", Nodes}, {"running_nodes", RunningNodes}]}],
     Bin -> {"permissions", [erlang:tuple_to_list(P) || P <- Bin ]}
+  catch
+    _X -> {?MODULE, <<"unknown error">>}
   end.
-  
+    
 % ConfigurePerm, WritePerm, ReadPerm
 extract_param(Name, Data) ->
   case proplists:get_value(erlang:list_to_binary(Name), Data) of
--- src/utils/testing.erl
+++ src/utils/testing.erl
@@ -9,3 +9,17 @@ turn_equal_to_json(Str) ->
       string:tokens(Line, "=")
     end, Set),
   lists:map(fun([K|V]) -> "{'"++K++"':'"++erlang:list_to_binary(V)++"'}" end, ArrSet).
+  
+deprecated_test() ->
+  Vhost = "/",  
+  Catcher = try rabint:call({rabbit_access_control, list_vhost_users, [Vhost]}) of
+    {badrpc, Mess} ->
+      io:format("----------Got back: badrc: ~p~n", [Mess]),
+      rabint:call({rabbit_access_control, list_users, []});
+    V -> V
+  catch
+    X -> 
+      io:format("Uh oh. have to deprecate: ~p~n", [X])
+  end,
+  io:format("Got: ~p~n", [Catcher]),
+  ok.
