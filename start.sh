f818ebe36d6644517249b105bfb175bc0124c099
Jason Dusek <jason.dusek@gmail.com>
2010-04-16 14:27:08 -0700

    Bashishm chokes on Ubuntu.

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd `dirname $0`
 make
-if [[ ! -f ebin/rest_app.boot ]]; then
+if [ ! -f ebin/rest_app.boot ]; then
 	make boot
 fi
 erl -pa $PWD/ebin -pa $PWD/deps/*/ebin -name alice -s reloader -boot alice $*
